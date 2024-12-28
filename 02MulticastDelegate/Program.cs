ef3830031f16b4c6baf49402c54e64231a829a7f
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-28 20:57:30 -0300

    multicast delegate

---
+++ 02MulticastDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿MeuDelegate delegateMulticast = new MeuDelegate(Metodo1);
+delegateMulticast += Metodo2;
+delegateMulticast += Metodo3;
+
+delegateMulticast("Ola multicast delegate!");
+
+delegateMulticast -= Metodo3;
+delegateMulticast("Metodo 3 removido");
+
+Console.ReadKey();
+
+static void Metodo1(string mensagem)
+{
+    Console.WriteLine("Metodo 1: " +mensagem);
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine("Metodo 2: " +mensagem);
+}
+
+static void Metodo3(string mensagem)
+{
+    Console.WriteLine("Metodo 3: " +mensagem);
+}
+
+public delegate void MeuDelegate(string mensagem);
