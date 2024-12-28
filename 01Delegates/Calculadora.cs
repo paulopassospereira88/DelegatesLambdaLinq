1501058fe456a04ed39d771289270e7ef1d81fc6
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-28 20:08:43 -0300

    Add project files.

---
+++ 01Delegates/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Delegates
+{
+    public class Calculadora
+    {
+        public static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Multiplicar (float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        public static float Dividir(float n1, float n2)
+        {
+            return n1 / n2;
+        }
+
+    }
+}
---
+++ 01Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using _01Delegates;
+
+DelegateCalculadora calc1 = new DelegateCalculadora(Calculadora.Somar);
+var resultado1 = calc1.Invoke(20, 30);
+Console.WriteLine($"Somar {resultado1}");
+
+DelegateCalculadora calc2 = Calculadora.Somar;
+var resultado2 = calc1.Invoke(20, 40);
+Console.WriteLine($"Somar {resultado2}");
+
+DelegateCalculadora calc3 = (float x, float y) => Calculadora.Dividir(x, y);
+var resultado3 = calc3.Invoke(40, 2);
+Console.WriteLine($"Dividir {resultado3}");
+
+Console.ReadKey();
+
+public delegate float DelegateCalculadora(float x, float y);
