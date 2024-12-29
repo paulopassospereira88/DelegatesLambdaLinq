d556cd11a8123b1bd279fcb62b601f83e48a8599
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-29 10:31:54 -0300

    Usando delegate do predicate

---
+++ 05DelegatePredicate/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Informe um numero inteiro :");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+// Predicate<int> delegatePar = VerificarPar;
+/*
+Predicate<int> delegatePar = delegate(int x)
+{
+    return x % 2 == 0;
+};
+*/
+
+Predicate<int> delegatePar = x => numero % 2 == 0;
+
+if (delegatePar(numero))
+{
+    Console.WriteLine($"O numero {numero} e PAR");
+}
+else
+{
+    Console.WriteLine($"O numero {numero} e IMPAR");
+}
+
+Console.ReadKey();
+
+static bool VerificarPar(int x)
+{
+    if (x % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
