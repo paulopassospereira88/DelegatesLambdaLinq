8920f397f7e49e781c25226eac71949ec113cf99
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-29 10:46:23 -0300

    Delegate Action

---
+++ 06DelegateAction/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Informe um numero inteiro");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+Action<int> dobra = x => Console.WriteLine(x * 2);
+dobra(numero);
+Duplicar(numero);
+
+Console.ReadKey();
+
+static void Duplicar(int numero)
+{
+    int resultado = numero * 2;
+    Console.WriteLine(resultado);
+}
