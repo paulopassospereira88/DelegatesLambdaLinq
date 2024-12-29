ad89dd769c8e79b67d7934a53e4743cb60a172ed
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-29 11:03:28 -0300

    delegate Func

---
+++ 07DelegateFunc/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite um numero : ");
+var numero = Convert.ToDouble(Console.ReadLine());
+
+Func<double, double> raizQuadrado = Math.Sqrt;
+
+var resultado = raizQuadrado(numero);
+
+Console.WriteLine("\nA raiz quadrada de " +numero+ " e : "  +resultado);
