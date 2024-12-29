59795f301c6ed1a48643855e4af9924a3eb62e9a
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-29 09:44:56 -0300

    funções anônimas

---
+++ 03MetodosAnonimos/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> nomes = new List<string>();
+nomes.Add("Maria");
+nomes.Add("Miriam");
+nomes.Add("Paulo");
+nomes.Add("Carlos");
+
+var resultado = nomes.Find(delegate(string nome)
+{
+    return nome.Equals("Maria");
+});
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
+
+static bool VerificaNomeNaLista(string nome)
+{
+    Console.WriteLine($"Consulta nome na lista : {nome}");
+    return nome.Equals("Paulo");
+}
