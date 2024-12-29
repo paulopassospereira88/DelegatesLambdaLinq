66591dbb6eb27e0d3b132e361f46484700a73c87
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-29 10:00:14 -0300

    expressao lambda

---
+++ 04ExpressaoLambda/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> nomes = new List<string>();
+nomes.Add("Maria");
+nomes.Add("Miriam");
+nomes.Add("Paulo");
+nomes.Add("Carlos");
+
+var resultado = nomes.Find(nome => nome.Equals("Maria"));
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
