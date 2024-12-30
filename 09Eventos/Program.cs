50539214b561de58a06f63edcdf77edfd29be446
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-30 17:25:07 -0300

    basico sobre eventos do C#

---
+++ 09Eventos/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("\nUsando o evento OnCriarPedido");
+
+var pedido = new Pedido();
+pedido.OnCriarPedido += Email.Enviar;
+pedido.OnCriarPedido += SMS.Enviar;
+
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+delegate void PedidoEventHandler();
+
+class Pedido
+{
+    public event PedidoEventHandler? OnCriarPedido;
+
+    public void CriarPedido()
+    {
+        Console.WriteLine("Pedido criado !!!");
+        if (OnCriarPedido != null)
+        {
+            OnCriarPedido();
+        }
+    }
+}
+
+class Email
+{
+    public static void Enviar()
+    {
+        Console.WriteLine("Enviando e-mail");
+    }
+}
+
+class SMS
+{
+    public static void Enviar()
+    {
+        Console.WriteLine("Enviando SMS");
+    }
+}
