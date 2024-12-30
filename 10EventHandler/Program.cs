64387a3b97f809bee1bfe14e6eb9d51cb0b9d8be
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-30 17:55:39 -0300

    basico sobre EventHandler

---
+++ 10EventHandler/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("\nUsando o evento OnCriarPedido");
+
+var pedido = new Pedido();
+pedido.OnCriarPedido += Email.Enviar;
+pedido.OnCriarPedido += SMS.Enviar;
+
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+class Pedido
+{
+    public event EventHandler? OnCriarPedido;
+
+    public void CriarPedido()
+    {
+        Console.WriteLine("Pedido criado !!!");
+        if (OnCriarPedido != null)
+        {
+            OnCriarPedido(this, EventArgs.Empty);
+        }
+    }
+}
+
+class Email
+{
+    public static void Enviar(object? sender, EventArgs args)
+    {
+        Console.WriteLine("Enviando e-mail");
+    }
+}
+
+class SMS
+{
+    public static void Enviar(object? sender, EventArgs args)
+    {
+        Console.WriteLine("Enviando SMS");
+    }
+}
