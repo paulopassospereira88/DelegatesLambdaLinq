6346b4e792f740f50f98370d44a2a930efacf9e6
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-31 12:55:03 -0300

    Basico sobre consulta Linq

---
+++ 13Linq/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> nomes = new List<string>()
+{
+    "Ana", "Maria", "Pedro", "Lair", "Paulo"
+};
+
+Console.WriteLine("Sintaxe de consulta");
+var resultado1 = from m in nomes where m.Contains('o') select m;
+ExibirResultado(resultado1);
+
+Console.WriteLine("Sintaxe de metodo");
+var resultado2 = nomes.Where(m => m.Contains('o')).ToList();
+ExibirResultado(resultado2);
+
+static void ExibirResultado(IEnumerable<string> enumerable)
+{
+    foreach (var item in enumerable)
+    {
+        Console.WriteLine(item);
+    }
+}
